e49ce04f7803e5d8041712edc6654f61c2e8ace4
Olga Vlasova <olgav1918@gmail.com>
2023-06-28 19:31:03 +0300

    last change

--- src/test/java/ApplyFormTest.java
+++ src/test/java/ApplyFormTest.java
@@ -17,20 +17,18 @@ public class ApplyFormTest {
     @BeforeAll
     static void setupAll() {
         WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-
-    }
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();   }
+      }
     @AfterAll
     static void teardown(){
         driver.quit();
-
     }
     @Test
      void shouldTestApplyForm() {
